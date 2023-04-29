e0c7ab4ec2716635262811c5c1887a8eb706cce4
ooooose <yuuki.oose1990@gmail.com>
2023-04-30 07:18:28 +0900

    [Add]indexアクションの設定

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ContactFormController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::prefix('contacts')
+    ->middleware(['auth'])
+    ->controller(ContactFormController::class)
+    ->group(function () {
+        Route::get('/', 'index')->name('index');
+});
 
 Route::get('/', function () {
     return view('welcome');
