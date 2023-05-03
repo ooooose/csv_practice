4e4a99d0a9912d1f6e2845c396a90dccc21fdef8
ooooose <yuuki.oose1990@gmail.com>
2023-05-03 13:12:48 +0900

    [Add]editアクション・ビューの作成

--- src/app/Http/Controllers/ContactFormController.php
+++ src/app/Http/Controllers/ContactFormController.php
@@ -83,7 +83,8 @@ class ContactFormController extends Controller
      */
     public function edit($id)
     {
-        //
+        $contact = ContactForm::find($id);
+        return view('contacts.edit', compact('contact'));
     }
 
     /**
--- src/resources/views/contacts/show.blade.php
+++ src/resources/views/contacts/show.blade.php
@@ -71,6 +71,11 @@
                                 </div>
                             </div>
                             </div>
+                            <div class="p-2 w-full">
+                                <form method='get' action="{{ route('edit', ['id' => $contact->id]) }}">
+                                    <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">編集する</button>
+                                </form>
+                            </div>
                         </div>
                     </section>
                 </div>
